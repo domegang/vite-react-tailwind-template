a555a74f1e0ba4186799e4d88e22a1b6077e6176
D <3818926+domegang@users.noreply.github.com>
2024-01-11 15:56:36 +0100

    template

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className='min-h-[100dvh] grid place-items-center'>
+      <h1 className='text-3xl text-center font-bold text-orange-800'>
+        react-tailwind-vite-template
+      </h1>
+    </div>
+  );
+}
+
+export default App;
